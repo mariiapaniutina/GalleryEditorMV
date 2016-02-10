83c321865bfc87245c508d0c97a785470925ccc1
Mariia Paniutina <mariia.paniutina@gmail.com>
2016-02-09 22:11:53 -0800

    added simple display option

---
+++ BackboneJS/js/views/displayView.js
@@ -0,0 +1,29 @@
+define(function(require) {
+
+    var Backbone = require('backbone');
+    var $ = require('jquery');
+    var Mustache = require('mustache');
+    
+    var template = require('text!../templates/display.tmpl');
+    var ImageModel = require('models/ImageModel');
+
+    var DisplayView = Backbone.View.extend({
+    	el: '#view_template',
+    	initialize: function(){
+    		this.model = new ImageModel();
+    		return this.render();
+    	},
+		render: function(){
+			var self = this;
+		 	
+		 	//var obj = {imageList: data.toJSON()};
+		 	var html = Mustache.to_html(template, this.model.toJSON());
+		 			
+		 			$(self.el).html(html);
+		 	
+
+		}
+    });
+
+    return DisplayView;
+});
